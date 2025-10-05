93632be4d97eba96641aed83723335a6b6dca13f
jackvo03 <142596613+jackvo03@users.noreply.github.com>
2025-10-06 00:38:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex items-center justify-center w-screen h-screen bg-black">
+    <main className="relative flex items-center justify-center w-screen h-screen bg-[#f6f6f6]">
       <div className="relative w-full h-full max-w-screen max-h-screen">
         <Image
           src="/laumann.png"
