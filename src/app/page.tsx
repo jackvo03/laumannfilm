7f19fcb86baf04ff9c697b6d3f3b0d3ad4f14af5
jackvo03 <142596613+jackvo03@users.noreply.github.com>
2025-10-06 00:36:16 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative w-screen h-screen">
-      <Image
-        src="/laumann.png"
-        alt="Laumann Film"
-        fill              // fyller hele parent (her: main)
-        className="object-cover" // dekker hele skjermen, cropper hvis nødvendig
-        priority
-      />
+    <main className="relative flex items-center justify-center w-screen h-screen bg-black">
+      <div className="relative w-full h-full max-w-screen max-h-screen">
+        <Image
+          src="/laumann.png"
+          alt="Laumann Film"
+          fill
+          className="object-contain"
+          priority
+        />
+      </div>
     </main>
   );
 }
